9760d80fd131c2c52afcc43e4a6ee076375cfe07
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-27 11:27:30 -0600

    commit tree 74ad821dc898d4c2e601ecc2aa33052d80892fca

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function func(item) {
+    return (item.result === 'W');
+    }
+
+function superbowlWin(record){
+    const element = record.find(func);
+    if (element) {return element.year}
+}
